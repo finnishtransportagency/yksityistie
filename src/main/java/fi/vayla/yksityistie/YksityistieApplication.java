61f37c6941762a1b6e51e99472da2f7fd7812d29
Pasi Savolainen <pasi.savolainen@dimenteq.fi>
2019-02-19 10:12:06 +0200

    major changes in structure

---
+++ src/main/java/fi/vayla/yksityistie/YksityistieApplication.java
@@ -0,0 +1,13 @@
+package fi.vayla.yksityistie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YksityistieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YksityistieApplication.class, args);
+	}
+
+}
--- src/main/resources/static/js/readme.txt
+++
@@ -1,1 +0,0 @@
-tämä kansio pitää luoda käsin
