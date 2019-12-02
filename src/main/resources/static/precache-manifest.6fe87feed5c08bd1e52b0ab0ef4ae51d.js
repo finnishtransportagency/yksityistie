1fbd5ba9245bfb578b5c5f685cbfb5e97ac28973
Tommi Lehtisaari <tommi.lehtisaari@sitowise.com>
2019-12-02 12:53:38 +0200

    new build for front-end

---
+++ src/main/resources/static/precache-manifest.6fe87feed5c08bd1e52b0ab0ef4ae51d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fdb562951bb5fc6d4aab4ea6eb43400",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a4ecee9831538ca6967e",
+    "url": "./static/css/2.28e23568.chunk.css"
+  },
+  {
+    "revision": "93448e366d8e48d3736c",
+    "url": "./static/css/main.7e2831a2.chunk.css"
+  },
+  {
+    "revision": "a4ecee9831538ca6967e",
+    "url": "./static/js/2.2a9c58b6.chunk.js"
+  },
+  {
+    "revision": "93448e366d8e48d3736c",
+    "url": "./static/js/main.65b71cbb.chunk.js"
+  },
+  {
+    "revision": "08b6406a0af222019104",
+    "url": "./static/js/runtime-main.e6a5d320.js"
+  },
+  {
+    "revision": "7645fd6e08e828c4617a3c342fe553e2",
+    "url": "./static/media/YT_banneri.7645fd6e.PNG"
+  },
+  {
+    "revision": "4ad50f4c10139bffc81629888733846f",
+    "url": "./static/media/oskari_placeholder.4ad50f4c.png"
+  }
+]);
--- src/main/resources/static/service-worker.js
+++ src/main/resources/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0340be82a00cd328ce2143a31233817e.js"
+  "./precache-manifest.6fe87feed5c08bd1e52b0ab0ef4ae51d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
