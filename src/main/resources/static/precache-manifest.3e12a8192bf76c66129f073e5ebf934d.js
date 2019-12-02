186fe8b4fcc2e87bb28235babf54ede3e70e52d0
Tommi Lehtisaari <tommi.lehtisaari@sitowise.com>
2019-12-02 15:57:44 +0200

    deploy

---
+++ src/main/resources/static/precache-manifest.3e12a8192bf76c66129f073e5ebf934d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80b3097bf843e7f4312cf1da343c6237",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7f13760703fc77762966",
+    "url": "./static/css/2.a35a71c9.chunk.css"
+  },
+  {
+    "revision": "9fecea7ab253971c1afc",
+    "url": "./static/css/main.7e2831a2.chunk.css"
+  },
+  {
+    "revision": "7f13760703fc77762966",
+    "url": "./static/js/2.d88f5dd2.chunk.js"
+  },
+  {
+    "revision": "9fecea7ab253971c1afc",
+    "url": "./static/js/main.0bca6f3a.chunk.js"
+  },
+  {
+    "revision": "08b6406a0af222019104",
+    "url": "./static/js/runtime-main.e6a5d320.js"
+  },
+  {
+    "revision": "7645fd6e08e828c4617a3c342fe553e2",
+    "url": "./static/media/YT_banneri.7645fd6e.PNG"
+  },
+  {
+    "revision": "4ad50f4c10139bffc81629888733846f",
+    "url": "./static/media/oskari_placeholder.4ad50f4c.png"
+  }
+]);
