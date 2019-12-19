08f7417721d0072e8bbec20ba90e7dad00e585f6
Tommi Lehtisaari <tommi.lehtisaari@sitowise.com>
2019-12-19 16:27:06 +0200

    debug logs for server

--- src/main/java/fi/vayla/yksityistie/api/PrivateRoadController.java
+++ src/main/java/fi/vayla/yksityistie/api/PrivateRoadController.java
@@ -41,11 +41,16 @@ public class PrivateRoadController {
         }
 
         byte[] pdf = new byte[0];
+        System.out.println("BEGINIGN OF PDF CREATION");
         try {
             pdf = pdfServise.createPdf(maintenanceAssociation);
         } catch (Exception exception) {
           //exeption
+            System.out.println("PDF ERROR");
+            System.out.println(exception);
         }
+
+        System.out.println("PDF DONE");
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_PDF);
